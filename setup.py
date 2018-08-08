8f702976c1c929a1fe2ad09a925ef918204e758e
Sheng Jie <sheng_mia@cat.com>
2018-08-08 10:34:47 +0200

    edit setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
-    url = ""
+    url = "https://github.com/frthesea/test2.git"
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
